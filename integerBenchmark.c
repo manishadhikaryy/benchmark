563b8fab95e18b59d4ca6987bd291d0c1090022e
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-25 20:54:32 -0400

    added 5 * 10^9 multiplications and 2 * 10^9 division operations

--- integerBenchmark.c
+++ integerBenchmark.c
@@ -30,7 +30,19 @@ int main() {
         }
     }
 
+    // 2 * 10^9 division (of integer constants)
+    for (int i=0; i < (2 * pow(10,4)); i++) {
+        // (10^5) operations inside 2 * (10^4) operations give  2 * 10^9 operations
+        for(int j = 0; j < pow(10,5); j++) {
+            num = num2 / num1;
+        }
+    }
+
     
+
+
+
+
 
     return 0;
 
