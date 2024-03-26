590de43d28d0491e40094bb7a8376d245090834c
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-26 17:23:23 -0400

    64 bit floating point operation for floats

--- floatBenchmark.c
+++ floatBenchmark.c
@@ -13,5 +13,15 @@ int main() {
     register double num2 = 1.2;
 
     double time_taken = 0.0;
+
+    //10^10 additions of double
+    for (int i = 0; i < pow(10, 5); i++) {
+        // (10 ^ 5) * (10 ^ 5) = 10 ^ 10 Operation (additions)
+        for(int j = 0; j < pow(10,5); j++){
+            num = num1 + num2;
+        }
+    }
+
+
     return 0; 
 }
