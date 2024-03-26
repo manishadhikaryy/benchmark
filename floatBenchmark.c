fce84e94bea79eee54de0ad39a3b837d2cdc7119
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-26 17:25:39 -0400

    multiplication operations for float

--- floatBenchmark.c
+++ floatBenchmark.c
@@ -22,6 +22,18 @@ int main() {
         }
     }
 
+    // 5 * 10^9 multiplications 
+    for (int i=0; i < (5 * pow(10,4)); i++) {
+        // (10^5) operations inside 5 * (10^4) operations give  5 * 10^9 operations
+        for(int j = 0; j < pow(10,5); j++) {
+            num = num1 * num2;
+        }
+    }
+
+    
+
+
+
 
     return 0; 
 }
