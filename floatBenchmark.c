da791cefb71a90a0eccf1442490d596ba88c4351
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-26 17:21:36 -0400

    assigned varibales for floats and initialized time

--- floatBenchmark.c
+++ floatBenchmark.c
@@ -2,3 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+
+int main() {
+    // Initialize time
+    clock_t initial_time = clock();
+
+    // set up registers
+    register double num;
+    register double num1 = 1.1;
+    register double num2 = 1.2;
+
+    double time_taken = 0.0;
+    return 0; 
+}
