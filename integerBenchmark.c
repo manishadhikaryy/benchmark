27e733134d754c87edbd50555bcacfebe32abf4f
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-25 20:20:10 -0400

    added initialization of time and register variables in the main function

--- integerBenchmark.c
+++ integerBenchmark.c
@@ -3,5 +3,16 @@
 #include <time.h> 
 #include <math.h>
 
+int main{
+    // Initialize time
+    clock_t initial_time = clock();
 
+    // set up registers
+    register int num;
+    register int num1 = 2;
+    register int num2 = 4;
+    
+    
+}
+ 
 
