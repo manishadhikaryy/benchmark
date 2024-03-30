66134900c95afdf71f201d4b53b5721f40547bba
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-29 23:39:31 -0400

    wrote into file2.txt

--- harddrive_two.c
+++ harddrive_two.c
@@ -10,5 +10,19 @@ int main()
 
     int seekByte = 10000;
     long long int totalBytes = 0;
+
+    FILE *fp = fopen("file2.txt", "w");
+
+   // write file into every 10000 bytes   
+    while (totalBytes < pow(10, 9)){
+        fseek(fp, seekByte - 1 , SEEK_CUR);
+        fputc('\0', fp);
+        totalBytes += seekByte;
+    }
+    fclose(fp);
+
+
+
+
     return 0;
 }
