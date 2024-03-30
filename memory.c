c2f19cdd2555a401049dea8fbbb6a762672973a5
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-29 22:11:51 -0400

    Initialize time tracking and array creation

--- memory.c
+++ memory.c
@@ -3,3 +3,12 @@
 #include <math.h>
 #include <time.h>
 
+int main()
+{
+    // get starting time
+    clock_t initial_time = clock(); 
+    double time_taken = 0.0;
+    long size = 5 * pow(10,4);
+    int nums[size];
+
+
