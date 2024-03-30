0ef611892a94edd4a69fa950bb15887b5512cdec
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-29 23:36:49 -0400

    initialized time and totalbytes varibales

--- harddrive_two.c
+++ harddrive_two.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+
+int main()
+{
+    clock_t initial_time = clock(); /* get starting time */
+    double time_taken = 0.0;
+
+    int seekByte = 10000;
+    long long int totalBytes = 0;
+    return 0;
+}
