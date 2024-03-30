2ed49578c9ee473a8ddba7effe987c158b2eca40
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-29 23:02:56 -0400

    initiated variables and time

--- harddrive_one.c
+++ harddrive_one.c
@@ -4,5 +4,14 @@
 #include <time.h>
  
 int main() {
+    clock_t initial time = clock();
+    double total_time_taken; 
+
+    // write file
+    int seekByte = 100;
+    long long int totalBytes;
+    
+
+
     return 0;
 }
