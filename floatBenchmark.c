47bbc56db09b55a236ec35850c27fbd60d7371a0
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-03-30 21:39:31 -0400

    the pow function was taking a long time in processing, so changed it instead

--- floatBenchmark.c
+++ floatBenchmark.c
@@ -15,31 +15,31 @@ int main() {
     double time_taken = 0.0;
 
     //10^10 additions of double
-    for (int i = 0; i < pow(10, 5); i++) {
+    for (int i = 0; i < 100000; i++) {
         // (10 ^ 5) * (10 ^ 5) = 10 ^ 10 Operation (additions)
-        for(int j = 0; j < pow(10,5); j++){
+        for(int j = 0; j < 100000; j++){
             num = num1 + num2;
         }
     }
 
     // 5 * 10^9 multiplications 
-    for (int i=0; i < (5 * pow(10,4)); i++) {
+    for (int i=0; i < (5 * 10000); i++) {
         // (10^5) operations inside 5 * (10^4) operations give  5 * 10^9 operations
-        for(int j = 0; j < pow(10,5); j++) {
+        for(int j = 0; j < 100000; j++) {
             num = num1 * num2;
         }
     }
 
     // 2 * 10^9 division
-    for (int i=0; i < (2 * pow(10,4)); i++) {
+    for (int i=0; i < (2 * 10000); i++) {
         // (10^5) operations inside 2 * (10^4) operations give  2 * 10^9 operations
-        for(int j = 0; j < pow(10,5); j++) {
+        for(int j = 0; j < 100000; j++) {
             num = num2 / num1;
         }
     }
     
     clock_t final_time = clock();
-    time_taken += (double) (final_time - initial_time) / CLOCKS_PER_SEC;
+    time_taken = (double) (final_time - initial_time) / CLOCKS_PER_SEC;
     // print final time 
     printf(" Benchmark time for the double operation is: %lf s", time_taken);
 
